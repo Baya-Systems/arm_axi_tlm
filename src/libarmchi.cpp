1728d8f649a7c0bf471f33d5ed96d2b7e27e53cf
Canlin Zhang <212407314+canlin-zhang@users.noreply.github.com>
2025-11-06 15:45:40 -0800

    Correct cpp includes for changed directory

--- src/libarmchi.cpp
+++ src/libarmchi.cpp
@@ -45,7 +45,7 @@
 #include <string>
 #include <vector>
 
-#include <include/ARM/TLM/arm_chi_payload.h>
+#include <ARM/TLM/arm_chi_payload.h>
 
 #ifdef ARM_TLM_ENABLE_VALGRIND
 #include <valgrind/memcheck.h>
